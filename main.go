b6eb91f7850d61605a27e616bbfd948159d6868d
Cristian Aldea <aldea.cristian@outlook.com>
2022-03-19 19:07:45 -0400

    add all tests
    also added .idea files

--- main.go
+++ main.go
@@ -8,35 +8,86 @@ import (
 )
 
 type Test struct {
-	url  string
+	name string
+	urls []string
 	code int
 	body string
 }
 
 var tests = []Test{
-	{url: "https://www.cristianaldea.com", code: 200, body: "app-content"},
-	{url: "https://cristianaldea.com", code: 200, body: "app-content"},
+	{
+		name: "Main website",
+		urls: []string{
+			"https://www.cristianaldea.com",
+			"https://cristianaldea.com",
+			"http://www.cristianaldea.com",
+		},
+		code: 200,
+		body: "Today is a gift!",
+	},
+	{
+		name: "Starlog",
+		urls: []string{
+			"https://blog.cristianaldea.com",
+			"http://blog.cristianaldea.com",
+		},
+		code: 200,
+		body: "Welcome to ⭐ Starlog ⭐",
+	},
+	{
+		name: "VoteIt",
+		urls: []string{
+			"https://voteit.cristianaldea.com",
+			"http://voteit.cristianaldea.com",
+			"https://voteit.cristianaldea.com/create-poll",
+		},
+		code: 200,
+		body: "The place to create poll easily, quickly, and with no signup!",
+	},
+	{
+		name: "VoteIt API",
+		urls: []string{
+			"https://api.voteit.cristianaldea.com/api/healthz",
+		},
+		code: 204,
+		body: "",
+	},
+	{
+		name: "Mull Recognition",
+		urls: []string{
+			"https://mull.cristianaldea.com",
+			"http://mull.cristianaldea.com",
+		},
+		code: 200,
+		body: "A standalone application for detecting waste!",
+	},
 }
 
 func main() {
 	log.Println("Testing")
 
 	for _, test := range tests {
-		log.Printf("main - Testing %s", test.url)
-		resp, err := http.Get(test.url)
-		if err != nil {
-			log.Fatalf("main - ERROR - %s: Failed to request url")
+		log.Printf("main - %s - Test started", test.name)
+
+		for _, url := range test.urls {
+			resp, err := http.Get(url)
+			if err != nil {
+				log.Fatalf("main - %s - ERROR: Failed to request url", test.name)
+			}
+
+			if test.code != resp.StatusCode {
+				log.Fatalf("main - %s - ERROR: Wrong status code (Expected: %d, Received: %d)", test.name, test.code, resp.StatusCode)
+			}
+
+			rawBody, _ := ioutil.ReadAll(resp.Body)
+			respBody := string(rawBody)
+
+			if test.body != "" && !strings.Contains(respBody, test.body) {
+				log.Fatalf("main - %s - ERROR: Body doesn't contain expected content: \"%s\"", test.name, test.body)
+			}
 		}
 
-		if test.code != resp.StatusCode {
-			log.Fatalf("main - ERROR - %s: Wrong status code (Expected: %d, Received: %d)", test.url, test.code, resp.StatusCode)
-		}
+		log.Printf("main - %s - Test completed successfully", test.name)
 
-		rawBody, _ := ioutil.ReadAll(resp.Body)
-		respBody := string(rawBody)
-
-		if !strings.Contains(respBody, test.body) {
-			log.Fatalf("main - ERROR - %s: Body doesn't contain \"%s\"", test.url, test.body)
-		}
 	}
 }
